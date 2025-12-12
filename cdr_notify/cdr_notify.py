3bfb1bcf131663a6fccd7377ec33ffa427d8dc40
talibdejon <tolib777@gmail.com>
2025-12-12 10:53:07 +0300

    edited full path, kept filename everywhere

--- cdr_notify/cdr_notify.py
+++ cdr_notify/cdr_notify.py
@@ -3,23 +3,24 @@ import os
 import time
 
 import utils
-from config import WATCH_DIR, CHECK_INTERVAL
+from config import CDR_FOLDER, CHECK_INTERVAL
 
 
 def main() -> None:
     logging.info("Starting CDR notify service")
-    logging.info("Watching directory: %s", WATCH_DIR)
+    logging.info("Watching folder: %s", CDR_FOLDER)
 
     while True:
         try:
-            files = os.listdir(WATCH_DIR)
+            files = os.listdir(CDR_FOLDER)
+            logging.info("Files detected: %s", files)
         except Exception:
-            logging.exception("Failed to list directory %s", WATCH_DIR)
+            logging.exception("Failed to list directory %s", CDR_FOLDER)
             time.sleep(CHECK_INTERVAL)
             continue
 
         for filename in files:
-            full_path = os.path.join(WATCH_DIR, filename)
+            full_path = os.path.join(CDR_FOLDER, filename)
 
             if not os.path.isfile(full_path):
                 continue
@@ -27,14 +28,14 @@ def main() -> None:
             try:
                 file_hash = utils.calculate_hash(full_path)
             except Exception:
-                logging.exception("Failed to calculate hash for %s", full_path)
+                logging.exception("Failed to calculate hash for %s", filename)
                 continue
 
             if utils.get_hash(file_hash):
                 continue
 
-            if not utils.send_email(full_path):
-                logging.error("Failed to send email for %s", full_path)
+            if not utils.send_email(full_path, filename=filename):
+                logging.error("Failed to send email for %s", filename)
                 continue
 
             utils.set_hash(filename, file_hash, utils.FileStatus.SENT)
