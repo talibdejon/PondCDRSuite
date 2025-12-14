9726335edda4b172a001c75614f8e507565d35fc
talibdejon <tolib777@gmail.com>
2025-12-14 18:25:58 +0300

    Remove polling loop

--- cdr_notify/cdr_notify.py
+++ cdr_notify/cdr_notify.py
@@ -1,12 +1,12 @@
 import logging
 import os
-import time
+import sys
 
 import database
 import utils
 
 
-def main() -> None:
+def main() -> int:
     logging.basicConfig(
         level=logging.INFO,
         format="%(asctime)s %(levelname)s %(message)s",
@@ -14,47 +14,46 @@ def main() -> None:
 
     cdr_folder = os.environ.get("CDR_FOLDER", "").strip()
     if not cdr_folder:
-        raise RuntimeError("CDR_FOLDER is not set")
-
-    check_interval = int(os.environ.get("CHECK_INTERVAL", "3600"))
+        logging.error("CDR_FOLDER is not set")
+        return 2
 
     database.init_db()
 
-    logging.info("Starting CDR notify service")
-    logging.info("Watching folder: %s", cdr_folder)
+    logging.info("Starting CDR notify")
+    logging.info("Scanning folder: %s", cdr_folder)
 
-    while True:
-        try:
-            files = os.listdir(cdr_folder)
-        except Exception:
-            logging.exception("Failed to list directory %s", cdr_folder)
-            time.sleep(check_interval)
+    try:
+        files = os.listdir(cdr_folder)
+    except Exception:
+        logging.exception("Failed to list directory %s", cdr_folder)
+        return 1
+
+    for filename in files:
+        if filename.startswith("."):
             continue
 
-        for filename in files:
-            full_path = os.path.join(cdr_folder, filename)
+        full_path = os.path.join(cdr_folder, filename)
+        if not os.path.isfile(full_path):
+            continue
 
-            if not os.path.isfile(full_path):
-                continue
+        try:
+            file_hash = utils.calculate_hash(full_path)
+        except Exception:
+            logging.exception("Failed to calculate hash for %s", filename)
+            continue
 
-            try:
-                file_hash = utils.calculate_hash(full_path)
-            except Exception:
-                logging.exception("Failed to calculate hash for %s", filename)
-                continue
+        if utils.get_hash(file_hash):
+            continue
 
-            if utils.get_hash(file_hash):
-                continue
+        if not utils.send_email(full_path, filename=filename):
+            logging.error("Failed to send email for %s", filename)
+            continue
 
-            if not utils.send_email(full_path, filename=filename):
-                logging.error("Failed to send email for %s", filename)
-                continue
+        utils.set_hash(filename, file_hash, utils.FileStatus.SENT)
+        logging.info("File processed successfully: %s", filename)
 
-            utils.set_hash(filename, file_hash, utils.FileStatus.SENT)
-            logging.info("File processed successfully: %s", filename)
-
-        time.sleep(check_interval)
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
